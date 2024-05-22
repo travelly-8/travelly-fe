7ceb254d26c5709348f00e6e7e00a64ef4f66d30
innerstella <suji0509@naver.com>
2024-05-22 20:39:40 +0900

    feat: 뒤로 가기 바

---
+++ src/components/back-bar/index.tsx
@@ -0,0 +1,11 @@
+import * as S from './BackBar.style'
+
+const BackBar = () => {
+  return (
+    <S.Wrapper>
+      <S.Icon src="/src/assets/common/arrow-left.svg" alt="뒤로 가기" />
+    </S.Wrapper>
+  )
+}
+
+export default BackBar
