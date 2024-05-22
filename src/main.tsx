bad88f897896e692564d7d77857a2ff5da092fdf
innerstella <suji0509@naver.com>
2024-05-22 18:16:47 +0900

    remove: 기본 CSS 파일 제거

--- src/main.tsx
+++ src/main.tsx
@@ -7,12 +7,14 @@ import { Provider } from 'react-redux'
 import App from './App.tsx'
 import store from './store/store.ts'
 
-import './index.css'
+import Layout from '@components/layout/index.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Layout>
+        <App />
+      </Layout>
     </Provider>
   </React.StrictMode>,
 )
