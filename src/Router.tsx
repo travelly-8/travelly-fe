5f75403593f43afc43d626dc12f2340553d6f1d3
GoM <honorrulru@gmail.com>
2024-05-23 15:31:55 +0900

    test page 삭제

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import HomePage from './pages/home'
-import Test from './pages/home/test/ButtonTest'
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/test" element={<Test />} />
       </Routes>
     </BrowserRouter>
   )
