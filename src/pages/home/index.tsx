45aafc88e6134863f74ce51a6bb08c4149799c9f
GoM <honorrulru@gmail.com>
2024-05-24 12:03:10 +0900

    test페이지 삭제

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,20 +1,7 @@
-import Input from '@components/input'
-
 export default function HomePage() {
   return (
-    <>
-      <Input inputType="name" placeholder="name" inputAccessedFor="login" />
-      <Input inputType="email" placeholder="email" inputAccessedFor="login" />
-      <Input
-        inputType="password"
-        placeholder="password"
-        inputAccessedFor="signup"
-      />
-      <Input
-        inputType="password_check"
-        placeholder="password_check"
-        inputAccessedFor="signup"
-      />
-    </>
+    <div>
+      <p>HELLO WORLD!</p>
+    </div>
   )
 }
