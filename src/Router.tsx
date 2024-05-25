7716bd74634c2f4dc0a541b287c88c59a3ef7d44
yohan11 <yipco77@naver.com>
2024-05-25 22:38:02 +0900

    fix: Router 페이지 싱크 맞춤

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,8 +1,9 @@
-import GlobalStyles from '@/styles/GlobalStyles'
+import GlobalStyles from '@/styles/GlobalStyles.tsx'
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import HomePage from './pages/home'
+import SearchPage from './pages/search'
 
 export default function AppRouter() {
   return (
@@ -10,6 +11,8 @@ export default function AppRouter() {
       <GlobalStyles />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/search" element={<SearchPage />} />
+        <Route path="/result" element={<div>result page</div>} /> // 임시페이지
       </Routes>
     </BrowserRouter>
   )
