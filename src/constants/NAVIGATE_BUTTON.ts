5b6356afc70a65df5524ea60472a6e59c227a8b5
JaeBeen95 <nin2348@gmail.com>
2024-05-25 16:41:35 +0900

    feat: travellee 객체 추가

--- src/constants/NAVIGATE_BUTTON.ts
+++ src/constants/NAVIGATE_BUTTON.ts
@@ -2,10 +2,19 @@ import chat from '@/assets/footer-navigation/annotation-heart.svg'
 import bookmark from '@/assets/footer-navigation/bookmark.svg'
 import home from '@/assets/footer-navigation/home-smile.svg'
 import myPage from '@/assets/footer-navigation/my-page.svg'
+import register from '@/assets/footer-navigation/package-plus.svg'
 
-export const FNB_TRAVELER = [
+export const FNB_TRAVELLER = [
   { img: chat, description: '챗', url: '/' },
   { img: bookmark, description: '찜', url: '/' },
   { img: home, description: '홈', url: '/' },
+  { img: myPage, description: 'my', url: '/' },
+] as const
+
+export const FNB_TRAVELLEE = [
+  { img: chat, description: '챗', url: '/' },
+  { img: bookmark, description: '찜', url: '/' },
+  { img: home, description: '홈', url: '/' },
+  { img: register, description: '등록', url: '/' },
   { img: myPage, description: 'my', url: '/' },
 ] as const
