9f7834ad5dab4e18d28f9631f8b99e671cdd7a9f
innerstella <suji0509@naver.com>
2024-05-25 15:13:45 +0900

    rename: modal -> sheet

--- src/pages/login/components/confirm-page/index.tsx
+++ src/pages/login/components/confirm-page/index.tsx
@@ -1,4 +1,4 @@
-import { ModalSliceState, modal } from '@/store/modal'
+import { SheetSliceState, sheet } from '@/store/sheet-slice'
 
 import RectangleButton from '@components/rectangle-button'
 import { useDispatch, useSelector } from 'react-redux'
@@ -7,18 +7,18 @@ import * as S from './ConfirmPage.style'
 
 const ConfirmPage = () => {
   const dispatch = useDispatch()
-  const modalReducer = useSelector(
-    (state: ModalSliceState) => state.modal.value,
+  const sheetReducer = useSelector(
+    (state: SheetSliceState) => state.sheet.value,
   )
   return (
     <S.Wrapper>
       <S.Img src="/src/assets/login/rocket.png" alt="회원 유형 선택 확인" />
-      <S.UpperText>{modalReducer.text}</S.UpperText>
+      <S.UpperText>{sheetReducer.text}</S.UpperText>
       <S.LowerText>회원 계정을 생성 하시겠습니까?</S.LowerText>
       <RectangleButton size="medium">계정 생성</RectangleButton>
       <S.BackButton
         onClick={() =>
-          dispatch(modal({ name: 'select-plan-confirm', status: false }))
+          dispatch(sheet({ name: 'select-plan-confirm', status: false }))
         }
       >
         &lt; 다시 선택하기
