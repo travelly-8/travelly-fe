6b394f5172208c8a38cd5230d83483d2c942e283
JaeBeen95 <nin2348@gmail.com>
2024-05-25 21:29:49 +0900

    docs: TODO 주석 추가

--- src/pages/browsing/components/sort-orders-sheet/index.tsx
+++ src/pages/browsing/components/sort-orders-sheet/index.tsx
@@ -2,6 +2,9 @@ import * as S from './SortOrdersSheet.style'
 
 const ORDER = ['최신순', '리뷰 많은 순', '평점순', '낮은 가격순'] as const
 
+//TODO: open, close 기능 필요
+//TODO: 버튼을 눌렀을 때, 정렬 버튼이 Order문구로 바뀌어야 하므로, ORDER 배열을 밖으로 빼서 관리해야할 것 같습니다
+
 function SortOrdersSheet() {
   return (
     <>
