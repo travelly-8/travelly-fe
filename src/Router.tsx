ec9c4667051bccb40f337d1fbd9a04a4c0d7af9b
innerstella <suji0509@naver.com>
2024-05-27 19:36:43 +0900

    feat: 회원가입 시작, 입력 페이지 라우터 등록

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,8 +4,9 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Login from './pages/login/login'
 import SelectPlanPage from './pages/login/select-plan'
-import SignupPage from './pages/signup'
+import { default as SignupStartPage } from './pages/signup'
 import SignupEndPage from './pages/signup/end'
+import SignupPage from './pages/signup/signup'
 
 export default function AppRouter() {
   return (
@@ -13,6 +14,7 @@ export default function AppRouter() {
       <GlobalStyles />
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/signup/start" element={<SignupStartPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/signup/end" element={<SignupEndPage />} />
         <Route path="/select-plan" element={<SelectPlanPage />} />
