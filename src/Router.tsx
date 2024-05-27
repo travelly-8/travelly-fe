41c875b1478ce9622ffd11a3388ee8f706f8c74d
GoM <honorrulru@gmail.com>
2024-05-27 21:25:59 +0900

    fix: window에서 font적용 안되던 오류 수정, Router경로 잘못 설정되어있는거 수정

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,7 +2,7 @@ import GlobalStyles from '@/styles/GlobalStyles'
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import Login from './pages/login/login'
+import HomePage from './pages/home'
 import SelectPlanPage from './pages/login/select-plan'
 import SignupPage from './pages/signup'
 import SignupEndPage from './pages/signup/end'
@@ -12,7 +12,7 @@ export default function AppRouter() {
     <BrowserRouter>
       <GlobalStyles />
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/signup/end" element={<SignupEndPage />} />
         <Route path="/select-plan" element={<SelectPlanPage />} />
