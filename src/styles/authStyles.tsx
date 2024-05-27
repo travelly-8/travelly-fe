ced11d62cafccb27e8cb4361c1c2929172fce37e
innerstella <suji0509@naver.com>
2024-05-27 19:35:48 +0900

    fix: 컨테이너 패딩 위계 조절

--- src/styles/authStyles.tsx
+++ src/styles/authStyles.tsx
@@ -6,6 +6,7 @@ export const Container = styled.div<{ isKeyboardOpen: boolean }>`
   margin-top: 2rem;
   background-color: var(--color-white);
   ${(props) => props.isKeyboardOpen && 'margin-top: -10rem;'}
+  padding: 0 2rem;
 `
 
 export const Header = styled.div`
@@ -32,7 +33,6 @@ export const InputContainer = styled.div`
   flex-direction: column;
   gap: 1.5rem;
   margin-bottom: 2rem;
-  padding: 0 2rem;
 `
 
 export const ButtonContainer = styled.div`
