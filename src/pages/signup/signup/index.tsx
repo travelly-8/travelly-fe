af80644a4308883d5831798ce8c9fd464f443c2c
GoM <honorrulru@gmail.com>
2024-05-27 10:45:13 +0900

    fix: 겹치는 로직 삭제, any타입 제거, form추가

--- src/pages/signup/signup/index.tsx
+++ src/pages/signup/signup/index.tsx
@@ -1,5 +1,6 @@
 import * as S from '@/styles/authStyles'
 import useKeyboardDetection from '@/utils/useKeyboardDetection'
+import BackBar from '@components/back-bar'
 import Input from '@components/input'
 import RectangleButton from '@components/rectangle-button'
 import { useEffect, useRef, useState } from 'react'
@@ -15,90 +16,70 @@ export default function Signup() {
 
   useEffect(() => {
     const checkInputs = () => {
-      const filled: any =
-        name &&
-        email &&
-        password &&
-        passwordCheck &&
+      const filled: boolean =
+        !!name &&
+        !!email &&
+        !!password &&
+        !!passwordCheck &&
         !document.querySelector('p')
       setAllInputsFilled(filled)
     }
 
     checkInputs()
-
-    setName((prevName) => {
-      checkInputs()
-      return prevName
-    })
-    setEmail((prevEmail) => {
-      checkInputs()
-      return prevEmail
-    })
-    setPassword((prevPassword) => {
-      checkInputs()
-      return prevPassword
-    })
-    setPasswordCheck((prevPasswordCheck) => {
-      checkInputs()
-      return prevPasswordCheck
-    })
-
-    const listener = () => {
-      checkInputs()
-    }
-    document.addEventListener('input', listener)
-
-    return () => {
-      document.removeEventListener('input', listener)
-    }
   }, [name, email, password, passwordCheck])
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+  }
 
   return (
     <S.Container isKeyboardOpen={isKeyboardOpen}>
-      <S.Header>
-        <S.ArrowIcon src="/src/assets/common/arrow-left.svg" alt="Back" />
-      </S.Header>
+      <BackBar />
       <S.Title>이메일로 회원가입</S.Title>
-      <S.InputContainer>
-        <Input
-          inputType="name"
-          placeholder="name"
-          inputAccessedFor="signup"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <Input
-          inputType="email"
-          placeholder="email"
-          inputAccessedFor="signup"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Input
-          inputType="password"
-          placeholder="password"
-          inputAccessedFor="signup"
-          value={password}
-          inputRef={passwordInputRef}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Input
-          inputType="password_check"
-          placeholder="password_check"
-          inputAccessedFor="signup"
-          value={passwordCheck}
-          passwordValue={passwordInputRef.current?.value}
-          onChange={(e) => setPasswordCheck(e.target.value)}
-        />
-      </S.InputContainer>
-      <S.ButtonContainer>
-        <RectangleButton
-          color={allInputsFilled ? 'primary' : 'disabled'}
-          size="large"
-        >
-          회원 가입
-        </RectangleButton>
-      </S.ButtonContainer>
+      <form onSubmit={handleSubmit}>
+        <S.InputContainer>
+          <Input
+            inputType="name"
+            placeholder="name"
+            inputAccessedFor="signup"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <Input
+            inputType="email"
+            placeholder="email"
+            inputAccessedFor="signup"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            inputType="password"
+            placeholder="password"
+            inputAccessedFor="signup"
+            value={password}
+            inputRef={passwordInputRef}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Input
+            inputType="password_check"
+            placeholder="password_check"
+            inputAccessedFor="signup"
+            value={passwordCheck}
+            passwordValue={passwordInputRef.current?.value}
+            onChange={(e) => setPasswordCheck(e.target.value)}
+          />
+        </S.InputContainer>
+        <S.ButtonContainer>
+          <RectangleButton
+            type="submit"
+            color={allInputsFilled ? 'primary' : 'disabled'}
+            size="large"
+            disabled={!allInputsFilled}
+          >
+            회원 가입
+          </RectangleButton>
+        </S.ButtonContainer>
+      </form>
     </S.Container>
   )
 }
