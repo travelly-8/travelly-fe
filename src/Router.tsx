6193aec01042a8765ef403f0229c6dd7d7a6aca5
innerstella <suji0509@naver.com>
2024-05-27 23:24:58 +0900

    feat: 로그인 링크 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,7 @@ import GlobalStyles from '@/styles/GlobalStyles'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import HomePage from './pages/home'
+import LoginPage from './pages/login/login'
 import SelectPlanPage from './pages/login/select-plan'
 import SignupStartPage from './pages/signup'
 import SignupEndPage from './pages/signup/end'
@@ -14,6 +15,7 @@ export default function AppRouter() {
       <GlobalStyles />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/signup/start" element={<SignupStartPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/signup/end" element={<SignupEndPage />} />
