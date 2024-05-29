5e8c70a41952661bd127cb5e848f5671074b48be
innerstella <suji0509@naver.com>
2024-05-30 00:50:04 +0900

    feat: footer navigation 연결

---
+++ src/pages/mypage/Mypage.style.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const FooterWrapper = styled.div`
+  width: 100%;
+  position: fixed;
+  bottom: 0%;
+`
--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,7 +1,10 @@
+import FooterNavigation from '@components/footer-navigation'
+
 import Dashboard from './components/dashboard'
 import MypageHeader from './components/mypage-header'
 import ProfileTab from './components/profile-tab'
 import data from './dummyData.json'
+import * as S from './Mypage.style'
 
 export default function MyPage() {
   const { email, nickname, coin, imageUrl, role, reviews } = data
@@ -10,6 +13,9 @@ export default function MyPage() {
       <MypageHeader />
       <ProfileTab data={{ email, nickname, imageUrl }} />
       <Dashboard data={{ role, coin, reviews }} />
+      <S.FooterWrapper>
+        <FooterNavigation />
+      </S.FooterWrapper>
     </>
   )
 }
