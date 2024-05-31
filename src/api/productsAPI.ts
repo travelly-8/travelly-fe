6d93d4c5c69ed7e7f7a3dcfdcedfe63c4ad7fa8b
joonyoungchoi0801 <joeplay0801@gmail.com>
2024-06-01 03:41:28 +0900

    feat: 검색,카테고리별, 필터링, 상품, 정렬 api 연결 완료

--- src/api/productsAPI.ts
+++ src/api/productsAPI.ts
@@ -1,5 +1,8 @@
 import { API_PRODUCTS } from '@/constants/API'
-import type { IPostProduct } from '@/types/postProductData.type'
+import type {
+  IPostProduct,
+  ISearchProductsData,
+} from '@/types/postProductData.type'
 
 import instance from './instance'
 
@@ -42,4 +45,40 @@ export const putProductDetail = (productId: number, data: IPostProduct) => {
     url: API_PRODUCTS.PRODUCTS_DETAIL(productId),
     data: data,
   })
+}
+
+export const getSearchProducts = (data: ISearchProductsData) => {
+  const params = createParams(data)
+  return instance({
+    method: 'GET',
+    url: API_PRODUCTS.PRODUCTS_SEARCH,
+    params,
+  })
+}
+
+const createParams = (data: ISearchProductsData) => {
+  const params: { [key: string]: string | number } = {}
+
+  Object.keys(data).forEach((key) => {
+    const value = data[key as keyof ISearchProductsData]
+    if (value !== undefined) {
+      params[key] = value
+    }
+  })
+
+  // params.page = data.page
+  // params.size = data.size
+  // if (data.sortField) params.sortField = data.sortField
+  // if (data.sortType) params.sortType = data.sortType
+  // if (data.keyword) params.keyword = data.keyword
+  // if (data.cityCode) params.cityCode = data.cityCode
+  // if (data.contentType) params.contentType = data.contentType
+  // if (data.startDate) params.startDate = data.startDate
+  // if (data.endDate) params.endDate = data.endDate
+  // if (data.startTime) params.startTime = data.startTime
+  // if (data.endTime) params.endTime = data.endTime
+  // if (data.minPrice !== undefined) params.minPrice = data.minPrice
+  // if (data.maxPrice !== undefined) params.maxPrice = data.maxPrice
+
+  return params
 }
--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
 
+import { getSearchProducts } from '@/api/productsAPI'
 import { mockData2 } from '@/constants/MOCK_DATA'
+import useGetSearchProducts from '@/hooks/api/productsAPI/useGetSearchProducts'
 import FilteringSheet from '@/pages/products/components/filtering-sheet'
 import SortOrdersSheet from '@/pages/products/components/sort-orders-sheet'
 import { SheetSliceState, sheet } from '@/store/sheet-slice.ts'
+import { ISearchProductsData } from '@/types/postProductData.type'
 
 import ProductCard from '@components/product-card'
 import ProductHeader from '@components/product-header'
 import { useDispatch, useSelector } from 'react-redux'
+import { useLocation } from 'react-router-dom'
 
 import * as S from './ProductsPage.style'
 
@@ -15,6 +19,39 @@ function ProductsPage() {
   const [isKebabClicked, setIsKebabClicked] = useState(false)
 
   const dispatch = useDispatch()
+  const location = useLocation()
+  const queryParams = new URLSearchParams(location.search)
+
+  const input = queryParams.get('input')
+  const type = queryParams.get('type')
+  const minPrice = queryParams.get('minPrice')
+  const maxPrice = queryParams.get('maxPrice')
+  // const startTime = queryParams.get('startTime')
+  // const endTime = queryParams.get('endTime')
+  const date = queryParams.get('date')
+  const city = queryParams.get('city')
+  const sort = queryParams.get('sort')
+
+  const queryData: ISearchProductsData = {
+    page: 1,
+    size: 10,
+    sortField: sort || undefined,
+    sortType: undefined,
+    keyword: input || undefined,
+    cityCode: city || undefined,
+    contentType:
+      type === '0' || type === undefined ? undefined : (type as string),
+    startDate: date || undefined,
+    endDate: date || undefined,
+    startTime: undefined,
+    endTime: undefined,
+    minPrice: Number(minPrice) || undefined,
+    maxPrice: Number(maxPrice) || undefined,
+  }
+
+  const { data } = useGetSearchProducts(queryData, getSearchProducts, queryData)
+  console.log(data?.content)
+
   const sheetReducer = useSelector(
     (state: SheetSliceState) => state.sheet.value,
   )
