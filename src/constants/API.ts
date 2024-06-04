5748a145eac75c51189ed99620f70f098cbda5f3
joonyoungchoi0801 <joeplay0801@gmail.com>
2024-06-05 00:45:16 +0900

    fix: api 경로 수정

--- src/constants/API.ts
+++ src/constants/API.ts
@@ -1,7 +1,7 @@
 export const API_PRODUCTS = Object.freeze({
   PRODUCTS: '/products/',
   PRODUCTS_DETAIL: (productId: number) => `/products/${productId}`,
-  PRODUCTS_SEARCH: '/products/search',
+  PRODUCTS_SEARCH: '/products/',
 })
 
 export const API_MEMBER = Object.freeze({
