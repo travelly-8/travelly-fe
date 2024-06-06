70464980d454d7daccaba238aafbfbf8b4edd556
innerstella <suji0509@naver.com>
2024-06-06 22:37:49 +0900

    feat: 회원 유형 선택 페이지 분기 처리 #104

--- src/pages/login/components/confirm-page/index.tsx
+++ src/pages/login/components/confirm-page/index.tsx
@@ -1,4 +1,5 @@
 import { SheetSliceState, sheet } from '@/store/sheet-slice'
+import { user } from '@/store/user-slice'
 
 import RectangleButton from '@components/rectangle-button'
 import { useDispatch, useSelector } from 'react-redux'
@@ -15,6 +16,7 @@ const ConfirmPage = () => {
 
   const handleCreateAccount = () => {
     navigate('/')
+    dispatch(user({ newUser: false }))
   }
 
   return (
