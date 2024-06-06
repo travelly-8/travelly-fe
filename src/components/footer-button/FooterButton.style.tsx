f853c82a976f3763602de7fa310c08458f8e73a5
innerstella <suji0509@naver.com>
2024-06-06 14:56:49 +0900

    feat: 리뷰 작성 UI

---
+++ src/components/footer-button/FooterButton.style.tsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  border-top: 0.1rem solid var(--color-gray-light);
+  background: var(--color-white);
+  padding: 1.2rem 0;
+`
+
+export const Button = styled.button`
+  display: flex;
+  width: 30.4rem;
+  height: 3.2rem;
+  padding: 1.1rem 2rem;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+  border-radius: 1.6rem;
+  background: var(--color-main);
+
+  color: var(--color-white);
+  text-align: center;
+  font-size: 1.4rem;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 2.38rem; /* 170% */
+`
