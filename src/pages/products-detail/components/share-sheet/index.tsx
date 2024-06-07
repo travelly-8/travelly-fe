72a67975fef55a611ca3b41176b59edca5cf6fbd
joonyoungchoi0801 <joeplay0801@gmail.com>
2024-06-08 01:56:16 +0900

    feat: 링크 복사하기

--- src/pages/products-detail/components/share-sheet/index.tsx
+++ src/pages/products-detail/components/share-sheet/index.tsx
@@ -3,6 +3,12 @@ import GrabSheet from '@components/grab-sheet'
 import * as S from './ShareSheet.style'
 
 function ShareSheet() {
+  const url = window.location.href
+
+  const copyUrl = () => {
+    navigator.clipboard.writeText(url)
+  }
+
   return (
     <GrabSheet name="share-sheet">
       <S.Wrapper>
@@ -11,7 +17,7 @@ function ShareSheet() {
           <S.Share>카카오톡으로 공유하기</S.Share>
         </S.ShareWrapper>
         <S.Divider />
-        <S.ShareWrapper>
+        <S.ShareWrapper onClick={copyUrl}>
           <S.LinkIcon />
           <S.Share>링크 복사하기</S.Share>
         </S.ShareWrapper>
