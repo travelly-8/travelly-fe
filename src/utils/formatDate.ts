2428a33e0dbf3123bce52e42370f3e5a40373276
JaeBeen95 <nin2348@gmail.com>
2024-06-13 15:02:21 +0900

    refactor: 모든 날짜 데이터값도 리턴할 수 있도록 수정

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,12 +1,13 @@
 import { IOperationDays } from '@/types/postProductData.type'
 
-export const getFirstAndLastDates = (arr: IOperationDays[] | undefined) => {
+export const getDateArray = (arr: IOperationDays[] | undefined) => {
   if (!arr || arr.length === 0) {
     return { firstDate: null, lastDate: null }
   }
 
   const firstDate = new Date(arr[0].date)
   const lastDate = new Date(arr[arr.length - 1].date)
+  const allDates = arr.map((item) => new Date(item.date))
 
-  return { firstDate, lastDate }
+  return { firstDate, lastDate, allDates }
 }
